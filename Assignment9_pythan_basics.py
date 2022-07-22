4f62f4d818423e6d536115db818f3b852c2ce640
Abhishek Narayan <abhisheknarayan73@gmail.com>
2022-07-22 23:24:04 +0530

    Add files via upload

---
+++ Assignment9_pythan_basics.py
@@ -0,0 +1,178 @@
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "id": "2f0b3ed0",
+   "metadata": {},
+   "source": [
+    "1. To what does a relative path refer?"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "1607bf33",
+   "metadata": {},
+   "source": [
+    "ANS - A relative path refers to the file related to current working directory."
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "55156bca",
+   "metadata": {},
+   "source": [
+    "2. What does an absolute path start with your operating system?"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "758bf50e",
+   "metadata": {},
+   "source": [
+    "ANS - Absolute path starts with root folder of the OS."
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "04106714",
+   "metadata": {},
+   "source": [
+    "3. What do the functions os.getcwd() and os.chdir() do?"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "46bdaa74",
+   "metadata": {},
+   "source": [
+    "By using os.getcwd()  method we can get the current working directory of the particular file.\n",
+    "By using os.chdir() method we can change the working directory in running time."
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "cc117b05",
+   "metadata": {},
+   "source": [
+    "4. What are the . and .. folders?"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "bba1c79a",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "markdown",
+   "id": "0c581a4e",
+   "metadata": {},
+   "source": [
+    "5. In C:\\bacon\\eggs\\spam.txt, which part is the dir name, and which part is the base name?"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "13b52426",
+   "metadata": {},
+   "source": [
+    "ANS - C:\\bacon\\eggs is the dir name and spam.txt is the base name."
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "0343eaee",
+   "metadata": {},
+   "source": [
+    "6. What are the three “mode” arguments that can be passed to the open() function?"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "b1e1b5d8",
+   "metadata": {},
+   "source": [
+    "ANS - 'r' for read only mode, 'rb' for read only in binary format and 'r+' for read and write mode"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "84628a6e",
+   "metadata": {},
+   "source": [
+    "7. What happens if an existing file is opened in write mode?"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "93296a2c",
+   "metadata": {},
+   "source": [
+    "ANS - its contents will be deleted and it will open as new file."
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "28da1287",
+   "metadata": {},
+   "source": [
+    "8. How do you tell the difference between read() and readlines()?"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "97653165",
+   "metadata": {},
+   "source": [
+    "ANS - read() will read the whole file at once whereas readlines() will read onle first character of the file."
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "579daff8",
+   "metadata": {},
+   "source": [
+    "9. What data structure does a shelf value resemble?"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "b830107a",
+   "metadata": {},
+   "source": [
+    "ANS - A dictionary value."
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "42d16cd1",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.7"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
